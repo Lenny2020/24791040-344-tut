bac6c50424630061b2236c2465b2314374044c0c
Brugman, L, Mr [24791040@sun.ac.za] <24791040@sun.ac.za>
2023-07-26 15:57:34 +0200

    [#16] javadocs

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,18 +1,25 @@
 
 import java.util.Scanner;
 
-
+/**
+This is a greeter class
+**/
 public class Greeter {
     public static void main(String args[]) {
         greeter();
     }
-
+/**
+Greets the person
+**/
     public static void greeter() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
         System.out.println("Hello " + scanner.nextLine());
-
+/**
+Gets the name
+@return the name
+**/
     private static string getName() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
