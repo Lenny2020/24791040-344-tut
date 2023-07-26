289257910ccbcc6249ad3a362bd1db4e8c25c4ed
Brugman, L, Mr [24791040@sun.ac.za] <24791040@sun.ac.za>
2023-07-26 15:33:45 +0200

    [#8] I are genuis i start own uni

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,12 @@
+java.util.Scanner;
 public class Greeter {
     public static void main(String args[]) {
 
+    }
+
+    private static string getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
     }
 }
